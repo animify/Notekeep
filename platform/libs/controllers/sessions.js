4d3634b8bd7f042320504588ca90041f8b2a2ed9
Stefan Mansson <st.mansson@icloud.com>
2017-03-17 13:50:11 +0000

    Added Pace
    Added pace for ajax request loading animation

--- platform/libs/controllers/sessions.js
+++ platform/libs/controllers/sessions.js
@@ -12,11 +12,10 @@ exports.userSessions = (req, res, callback) => {
 	const connection = req.app.get('connection')
 	connection.db.collection("sessions", function(err, collection){
 		console.log(req.user.username);
-		collection.find({"session.passport.user._id": req.user._id}).toArray((err, data) => {
-			session = data[0]
-			if (data.length == 0) return callback(101, "No sessions")
+		collection.find({"session.passport.user._id": req.user._id}).toArray((err, session) => {
+			if (session.length == 0) return callback(101, "No sessions")
 			console.log('data', session)
-			callback(null, session.session.passport)
+			callback(null, session[1].session.passport)
 		})
 	})
 }
